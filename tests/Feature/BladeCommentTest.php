851b9b20f359aa1ac1c3b0cd08370b9bd2eb1519
gwleuverink <willem@leuver.ink>
2024-08-15 22:49:30 +0200

    add tests for quotes in todo's

--- tests/Feature/BladeCommentTest.php
+++ tests/Feature/BladeCommentTest.php
@@ -26,6 +26,16 @@ it('compiles multiline Blade TODO comments', function () {
         HTML);
 });
 
+it('compiles quotes in Blade TODO comments', function () {
+    $html = Blade::render(<<< 'BLADE'
+    {{-- |TODO: Foo "Bar" baz --}}
+    BLADE);
+
+    expect($html)
+        ->not->toContain('{{-- TODO')
+        ->toContain('<span class="magic-todo-mark" data-todo="Foo &amp;quot;Bar&amp;quot; baz">');
+});
+
 it('compiles empty Blade TODO comments', function () {
     $html = Blade::render(<<< 'BLADE'
     {{-- |TODO: --}}
--- tests/Feature/HtmlCommentTest.php
+++ tests/Feature/HtmlCommentTest.php
@@ -26,6 +26,16 @@ it('compiles multiline HTML TODO comments', function () {
         HTML);
 });
 
+it('compiles quotes in Blade HTML comments', function () {
+    $html = Blade::render(<<< 'BLADE'
+    <!-- |TODO: Foo "Bar" baz -->
+    BLADE);
+
+    expect($html)
+        ->not->toContain('<!-- TODO')
+        ->toContain('<span class="magic-todo-mark" data-todo="Foo &amp;quot;Bar&amp;quot; baz">');
+});
+
 it('compiles empty HTML TODO comments', function () {
     $html = Blade::render('<!-- |TODO: -->');
 
