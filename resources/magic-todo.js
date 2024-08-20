d0a124ab83e981bab1572a0d5847c63426d4b786
gwleuverink <willem@leuver.ink>
2024-08-20 15:23:35 +0200

    support initialization after livewire conditional template render

--- resources/magic-todo.js
+++ resources/magic-todo.js
@@ -1,26 +1,43 @@
 import "./magic-todo.css";
 
-import tippy, { inlinePositioning } from "tippy.js";
+import tippy, { inlinePositioning, createSingleton } from "tippy.js";
 import "tippy.js/dist/tippy.css";
 import "tippy.js/themes/translucent.css";
 
-function init() {
-  tippy(".magic-todo", {
-    allowHTML: true,
-    theme: "translucent",
-    inlinePositioning: true,
-    plugins: [inlinePositioning],
-    content: (reference) => reference.dataset.todo || "todo",
-  });
+const config = {
+  allowHTML: true,
+  theme: "translucent",
+  inlinePositioning: true,
+  plugins: [inlinePositioning],
+  content: (reference) => reference.dataset.todo || "todo",
+};
 
-  tippy(".magic-todo-mark", {
-    allowHTML: true,
-    theme: "translucent",
-    inlinePositioning: true,
-    plugins: [inlinePositioning],
-    content: (reference) => reference.dataset.todo || "todo",
+function init() {
+  let wrapped = document.querySelectorAll(
+    ".magic-todo:not(.magic-todo--initialized)"
+  );
+  let marks = document.querySelectorAll(
+    ".magic-todo-mark:not(.magic-todo--initialized)"
+  );
+
+  tippy(wrapped, config);
+  tippy(marks, config);
+
+  // Add --initialized modifier so tooltip's aren't reinitialized each time
+  [...Array.from(wrapped), ...Array.from(marks)].forEach((element) => {
+    element.classList.add("magic-todo--initialized");
   });
 }
 
-document.addEventListener("DOMContentLoaded", init);
-document.addEventListener("livewire:init", init);
+// Initialize tooltips
+document.addEventListener("DOMContentLoaded", init); // Pageload
+
+// Livewire
+document.addEventListener("livewire:init", init); // wire:navigate
+document.addEventListener("livewire:init", () => {
+  // Lazy components
+  Livewire.hook("component.init", init);
+
+  // Rendered by after Blade conditional  - Must be a better way to do this?
+  Livewire.hook("element.init", init);
+});
